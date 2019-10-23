18a1517d9a7701c6b3dd562e0e8af9f80d6cc168
unasmera <tabea.easybox@gmail.com>
2019-10-23 20:09:24 +0200

    Selina hat die I_A Funktion zum laufen gebracht, so dass ein neuer Pfad geschrieben werden kann :-)

--- Version 1.py
+++ Version 1.py
@@ -48,108 +48,16 @@ def Pille ():
 
 #A: Es wird erklärt wer der Spielemacher ist
 def I_A ():
-    while "c" == generate(I_Ad,"",I_Ad["c"]):
-        pass
-
-def Anfang1 (x):
-    while True:
-        if x == "no" or x == "No":
-            print ("Ready Player One?")
-        elif  x== "yes" or x == "Yes":
-            print ("good")
-            return True
+    while "d" != generate(I_Ad,"",I_Ad["c"]):
+        while "c" == generate(I_Ad,"",I_Ad["c"]):
+            pass
+        if "d" == generate(I_Ad,"",I_Ad["c"]):
             break
-        else:
-            print ("Try again")
-            print ("Ready Player One?")
-        x = input().lower()
 
-def Name1 (name):
-    while True:
-        if name == "Yubin" or name == "Quirin Prummer" or name == "quirin":
-            print ("Hello Yubin, here's your first decision: Would you like the red pill, or the blue pill?")
-            return True
-            break
-        else:
-            print ("No, your real name")
-            name = input()
 
-def Pille1 (colour):
-    while True:
-        if colour == "red" or colour == "Red" or colour == "red pill" or colour == "Red Pill":
-            print ("You are now in the BANDERSNATCH. A never ending system of decisions.")
-            print ("Nothing you say or do is of your own free will. Your fate has long been")
-            print ("decided before you even thought about acting. There are people like you,")
-            print ("who are aware of the situation, and then there are people who are not.")
-            print ("Sleepwalkers unable to wake up if not for their own revelation.")
-            print ("Your mission, if you take it, is to terminate the decision maker") 
-            print ("The Sleepwalkers will do anything to stop you in doing so.")
-            print ("Choose how you would like to proceed:")
-            print ("A: Who is the decision maker")
-            print ("B: Who Am I?")
-            print ("C: I do not take the mission")
-            return True
-            break
-        elif colour == "blue" or colour == "Blue" or colour == "blue pill" or colour == "Blue Pill":
-            print ("As you wake up in your bed, everything seems normal at first. Dark orange light is")
-            print ("shimmering through your curtain covered window and sprinkles the matress with light yellow spots.")
-            print ("You get rid of the far too tiny piece of fabric that you used as a blanket")
-            print ("and roll over, off the grubby mattress and onto the concrete floor. Your head hurts")
-            print ("as though an elefant tried to dance Cha-Cha inside of it and you can't remember")
-            print ("how you got into your bed last night. Frantically you get up and start searching for")
-            print ("the mug of cold coffee that you must have left somewhere here yesterday evening.")
-            print ("A: Look on the construction you'd like to call desk")
-            print ("B: Look in the toilett")
-            return False
-            break
-        else:
-            print ("Choose a pill")
-            colour = input()
-
-def I_A1 (answer):
-    if answer == "A" or answer == "a":
-        print ("The decision maker is here and yet is not. It is a construct, an illusion if you will.")
-        print ("Unable to fully wrap ones mind about its existance, one can yet find its traces in")
-        print ("everything one does. It is almighty and yet has no power whatsoever. Take on the mission")
-        print ("and you will surely stumble upon its controversies.")
-        print ("Choose how you would like to proceed:")
-        print ("A: Who am I?")
-        print ("B: Start the mission!")
-        return False
-    elif answer == "B" or answer == "b":
-        print ("You are Player One")
-        print ("A: Who is the decision maker?")
-        print ("B: Start the mission!")
-        return True
-    else:
-        print ("ERROR 404")
-        print ("A: Who is the decision maker?")
-        print ("B: Who am I?")
-        print ("C: I do not take the mission")
-        answer = input()
 
 Anfang()
 Name ()
 Pille ()
 I_A()
 
-#print ("Ready Player One?")
-#a = input().lower()
-#if Anfang (a) == True:
- #   print ("Welcome to BANDERSNATCH")
- #   print ("Insert your name to play")
- #   b=input()
- #   if Name(b) == True:
- #       c=input()
- #       Pille(c)
-
-
-
-#print ("Ready Player One?")
-#a = input()
-#if [(a == ("yes")) or (a == ("Yes"))]:
- #   print ("good")
-#elif [(a == ("no")) or (a == ("No"))]:
- #   print ("Ready Player One?")
-           # else:
-            #    print ("Try again")
